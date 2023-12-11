cd6aa32cf206c02f8adff3b4e1e13269b5a4b4cb
IgiranezaMichel <igiramichael@gmail.com>
2023-12-11 10:15:33 +0200

    add getAllHospital

--- src/main/java/com/bookambulance/HospitalController.java
+++ src/main/java/com/bookambulance/HospitalController.java
@@ -1,8 +1,11 @@
 package com.bookambulance;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 import com.bookambulance.GraphQlModels.HospitalInput;
 import com.bookambulance.Model.Hospital;
@@ -12,7 +15,10 @@ import com.bookambulance.Services.HospitalServices;
 public class HospitalController {
 @Autowired private HospitalServices hospitalServices;
 @MutationMapping() public Hospital addHospital(@Argument(name = "input")HospitalInput HospitalInput)
-{return hospitalServices.saveOrUpdateData(HospitalInput);}
+{
+    return hospitalServices.saveOrUpdateData(HospitalInput);
+}
+
 @MutationMapping() public String deleHospital(@Argument long id){
     Hospital hospital=hospitalServices.findById(id);
     if(!(hospital==null)){
@@ -22,5 +28,9 @@ public class HospitalController {
     else{
         return "Hospital not found";
     }
+}
+@QueryMapping() public List<Hospital> getAllHospital()
+{
+    return hospitalServices.getAllData();
 }
 }
